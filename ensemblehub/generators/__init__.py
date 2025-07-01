f67a97e5aab903af1a25f23fa689dc5dd8ad8036
FzKuji <fzkuji@gmail.com>
2025-07-02 00:13:53 +0800

    delete vllm

--- ensemblehub/generators/__init__.py
+++ ensemblehub/generators/__init__.py
@@ -3,25 +3,17 @@ Generator modules for different inference backends
 """
 from .base import BaseGenerator, GenOutput
 from .hf_engine import HFGenerator
-from .vllm import VLLMGenerator
-from .pool import GeneratorPool
 
 # Optional Ray-based vLLM
 try:
-    from .vllm_ray import VLLMRayGenerator
     __all__ = [
         "BaseGenerator",
         "GenOutput", 
         "HFGenerator",
-        "VLLMGenerator",
-        "VLLMRayGenerator",
-        "GeneratorPool",
     ]
 except ImportError:
     __all__ = [
         "BaseGenerator",
         "GenOutput", 
         "HFGenerator",
-        "VLLMGenerator",
-        "GeneratorPool",
     ]
